232dc48dd05b8a69163509bdf80c2b76ff31bcc1
Luqman <luqmanhaqeemsamsul@gmail.com>
2020-10-07 15:02:53 +0800

    test

--- das-main.php
+++ das-main.php
@@ -2,6 +2,7 @@
 session_start();
 //dash-admin.php
 ?>
+<!-- test merge -->
 <h2>Welcome admin <?php $_SESSION['fullname'] ?></h2>
 <a href="forminsert.php">Insert record</a><br>
 <a href="update.php">Update/Delete record activity</a><br>
